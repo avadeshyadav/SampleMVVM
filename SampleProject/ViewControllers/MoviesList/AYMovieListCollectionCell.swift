1689560b91f75a1070a83e033a8c14247a7dbc38
Avadesh ji <avadesh.kumar@go-mac-avadeshk.local>
2018-07-29 09:20:34 +0530

    Added Initial project directory structure

---
+++ SampleProject/ViewControllers/MoviesList/AYMovieListCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  AYMovieListCollectionCell.swift
+//  SampleProject
+//
+//  Created by Avadesh Kumar on 29/07/18.
+//  Copyright © 2018 Avadesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class AYMovieListCollectionCell: UICollectionViewCell {
+    
+}
---
+++ SampleProject/ViewControllers/MoviesList/AYMoviesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AYMoviesListViewController.swift
+//  SampleProject
+//
+//  Created by Avadesh Kumar on 28/07/18.
+//  Copyright © 2018 Avadesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class AYMoviesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
